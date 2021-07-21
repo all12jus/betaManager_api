9662a7c17f7f0d2e8466ab31e12ef330a0be4b9e
Justin Allen <all12jus@gmail.com>
2021-07-21 12:48:02 -0400

    Wed Jul 21 12:48:02 EDT 2021

--- routes/beta.route.ts
+++ routes/beta.route.ts
@@ -5,6 +5,7 @@
     const utils = require('./../utils');
 
     const Beta = require("../models/beta.model");
+    const Tester = require("../models/tester.model");
     const router = express.Router();
 
     router.use([
@@ -27,6 +28,31 @@
                 }
                 res.json({err: err, item: betaItem});
             });
+        } catch (missingData) {
+            return res.status(400).json({ error: missingData });
+        }
+    });
+
+    router.post("/:beta_id/tester", async (req, res) => {
+        const beta = await Beta.findOne({
+            _id: req.params.beta
+        }).catch((err) =>{
+            return res.status(400).json({ error: err });
+        });
+
+        if (beta == null) {
+            return res.status(400).json({ error: "No beta found" });
+        }
+
+        try {
+            let result = utils.validateInputs(["last_name", "first_name", "email_address"], req.body);
+            const testerItem = new Tester(result);
+            testerItem.save((err) => {
+                if (err == null) {
+                    return res.status(500).json({error: "Database Error: " + err});
+                }
+                res.json({err: err, item: testerItem});
+            })
         } catch (missingData) {
             return res.status(400).json({ error: missingData });
         }
